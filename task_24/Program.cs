c4f1d75d777d9bc1ed805d5384f38cdaec1e8201
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-22 21:37:29 +0300

    First solution on the task 24

---
+++ task_24/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программуЮ которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// Например:
+// 7 --> 28
+// 4 --> 10
+// 8 --> 36
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int FindS(int num)
+{
+    int s = num * (num + 1) / 2;
+    return s;
+}
+
+int number = Prompt("Введите число: ");
+int summa = FindS(number);
+
+if(number >= 0)
+{
+    Console.WriteLine($"{number} --> {summa}");
+}
+else
+{
+    Console.WriteLine("Введенное число - отрицательное");
+}
