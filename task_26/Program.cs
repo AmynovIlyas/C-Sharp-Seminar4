48adab1a92e471c4726da5741facd161d7c8be87
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-23 00:26:08 +0300

    I want to sleep

---
+++ task_26/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количетсво цифр в числе
+// Например:
+// 456 --> 3
+// 78 --> 2
+// 89126 --> 5
+
+int Prompt()
+{
+    Console.WriteLine("Введите число: ");
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int HowManyNumber(int num)
+{
+    int count = 0;
+    if (num != 0)
+    {
+        while (num > 0 || num < 0)
+        {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+    return 1;
+}
+
+int number = Prompt();
+Console.WriteLine($"{number} --> {HowManyNumber(number)}");
+
---
+++ task_28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N
+// Например:
+// 4 --> 24
+// 5 --> 120
+
+int Prompt()
+{
+    Console.WriteLine("Введите число N: ");
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int FindMultiplication(int num)
+{
+    int sum = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+
+int number = Prompt();
+Console.WriteLine($"{number} --> {FindMultiplication(number)}");
+
+
