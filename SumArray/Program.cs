d669975b23f608664f47521a6839221cda703582
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-24 00:14:55 +0300

    Сolution task 30 and sumarray

---
+++ SumArray/Program.cs
@@ -0,0 +1,40 @@
+﻿// Сложение массивов
+
+int[] RandArr(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+int[] SumArray(int[] arr1, int[] arr2)
+{
+    int[] resArray = new int[arr1.Length];
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        resArray[i] = arr1[i] + arr2[i];
+    }
+    return resArray;
+}
+
+void Print(int[] arr)
+{
+    Console.Write("{");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"[{arr[i]}], ");
+    }
+    Console.Write($"[{arr[arr.Length - 1]}]" + "}");
+    Console.WriteLine();
+}
+
+const int lengthArr = 8;
+int[] array1 = RandArr(lengthArr);
+int[] array2 = RandArr(lengthArr);
+Print(array1);
+Print(array2);
+Print(SumArray(array1, array2));
+
---
+++ task_30/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу,которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+
+void FillArray(int[] collection)
+{
+    for(int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(0, 2);
+    }
+}
+
+// можно сделать иначе: вызывать метод и вводить число, равное количеству элементов в массиве
+// int FillArray(int collection)
+// {
+//     int[] arr = new int[collection];
+//     for(int i = 0; i < collection; i++)
+//     {
+//         arr[i] = new Random().Next(0, 2);
+//     }
+//     return array;
+// }
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    Console.Write("{");
+    for(int i = 0; i < length - 1; i++)
+    {
+        Console.Write($"{collection[i]}, ");
+    }
+    Console.Write($"{collection[length - 1]}" + "}");
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
